237c8fdbab24c7a8a3f882beb1767ba1d7cb7457
HEUSSERMichel <michel.heusser@hexagon.com>
2026-06-23 11:35:43 +0200

    Updated nvim config

--- nvim/.config/nvim/lua/plugins/catpuccin.lua
+++ nvim/.config/nvim/lua/plugins/catpuccin.lua
@@ -3,12 +3,19 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
+    opts = {
+      color_overrides = {
+        mocha = {
+          base = "#11111b",
+        },
+      },
+    },
+    build = ":CatppuccinCompile",
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "catppuccin-mocha",
     },
   },
 }
-
